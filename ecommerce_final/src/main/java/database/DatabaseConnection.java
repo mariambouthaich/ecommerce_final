94fc4b05db9beaf58a2b6a4a3d150e10190a3af0
Mariam Bouthaich <mariambouthaich@gmail.com>
2026-05-02 11:47:25 +0100

    Ajout interface client Personne 3 - catalogue, panier, commande, historique, profil

--- ecommerce_final/src/main/java/database/DatabaseConnection.java
+++ ecommerce_final/src/main/java/database/DatabaseConnection.java
@@ -3,9 +3,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 public class DatabaseConnection {
     // ── Paramètres de connexion ──────────────────────────────
-    private static final String URL      = "jdbc:mysql://localhost:3308/ecommerce";
+    private static final String URL      = "jdbc:mysql://localhost:3306/ecommerce";
     private static final String USER     = "root";
-    private static final String PASSWORD = "Salma_1337";
+    private static final String PASSWORD = "azert1234";
     // ────────────────────────────────────────────────────────
     private static Connection instance = null;
 
